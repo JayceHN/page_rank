875378ffb280ac54e784afea2211ec398ffa2b82
JayceHN <guillaume.calmant@student.uclouvain.be>
2016-04-20 15:19:12 +0200

    init

---
+++ main.java
@@ -0,0 +1,29 @@
+
+/**
+*  PageRank alogorithm
+*
+* @author Robin Hormaux & Guillaume Calmant
+* @version 0.1
+*/
+public class main{
+  // instance variables - replace the example below with your own
+  // private int x;
+
+  /**
+  * Constructor for objects of class main
+  */
+  public static void main(String [ ] args{
+
+  }
+
+  /**
+  * An example of a method - replace this comment with your own
+  *
+  * @param  y   a sample parameter for a method
+  * @return     the sum of x and y
+  */
+  public int import_csv(String csv_file{
+    // put your code here
+    return 42;
+  }
+}
